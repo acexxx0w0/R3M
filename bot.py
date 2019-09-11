618e10e82ef6dfb64fa4109af950fe3e3d299aa9
acexxx0w0 <acexxx0w0@gmil.com>
2019-09-11 23:28:18 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,10 @@
+import discord
+From discord.ext import commands
+
+bot = commands.bot{command_preFix='`'}
+
+@bot.event
+async def on_ready{}:
+    Print{"'>>Bot Is Online<<"}
+
+bot.run{'NjIwOTQwMzMzMTM1NDI5Njcy.XXkOsw.tIxUUjU-czh-V-dQZebUMfH20pg'}
